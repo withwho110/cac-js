c3de8b48e8a296a9215edd22d98312aa4fa66d55
withwho110 <128990442+withwho110@users.noreply.github.com>
2023-12-17 20:43:55 +0000

    container config done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
 console.log("vidhu");
+const accountId=14453;
+let accountEmail="vidhuchaudhary7@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+//accountId=2
+console.log(accountId)
+console.table({accountId,accountEmail,accountCity,accountState})
+
+
