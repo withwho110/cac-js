58b27fb9859ede7451d73406cbeb1c9de111c366
withwho110 <128990442+withwho110@users.noreply.github.com>
2023-12-19 20:51:55 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,11 @@
+let score="33abc"
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+let somenumber=33
+let stringNumber=String(somenumber);
+console.log(stringNumber);
+console.log(typeof stringNumber)
