8dec606403fb7b12bd807485e62a3d22beda8010
withwho110 <128990442+withwho110@users.noreply.github.com>
2023-12-20 19:03:03 +0000

    strings lesson

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+const name="vidhu";
+const repoCount=1
+console.log(name + repoCount + "Value");
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+const gameName=new String('VIDHUHC')
+console.log(gameName[0]);
+console.log(gameName.toUpperCase());
+const newString=gameName.substring(0,4)
+console.log(newString)
+const anotherString=gameName.slice(-8,4)
+
+
