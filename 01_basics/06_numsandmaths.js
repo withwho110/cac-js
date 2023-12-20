1bb22eb4040e6ec08a89bb8dd5bf5d26c2ae6185
withwho110 <128990442+withwho110@users.noreply.github.com>
2023-12-20 20:06:16 +0000

    math and num

---
+++ 01_basics/06_numsandmaths.js
@@ -0,0 +1,7 @@
+const score=400
+const balance =new Number(100)
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+const hundreds=10000000
+console.log(hundreds.toLocaleString('en-IN'));
